ead0cfb4d81c1324a25ed7e291b51b0bb554efbd
Aasim Shaikh <asimshaikh1993@gmail.com>
2022-02-21 18:14:08 +0530

    Added icons and updated footer with links and icons

--- components/footer.js
+++ components/footer.js
@@ -1,3 +1,6 @@
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <footer className="flex flex-col border-y-4">
@@ -6,10 +9,22 @@ const Footer = () => {
         <p>Copyright &#169; 2022 </p>
       </div>
       {/* Icons */}
-      <div className="flex justify-center p-3 gap-7">
-        <a>Github</a>
-        <a>Intagram</a>
-        <a>Linkdein</a>
+      <div className="flex justify-center py-4 gap-7">
+        <Link href="https://github.com/aasim-park?tab=repositories">
+          <a>
+            <AiFillGithub className="w-6 h-6" />
+          </a>
+        </Link>
+        <Link href="https://www.instagram.com/aasim08/">
+          <a>
+            <AiFillInstagram className="w-6 h-6" />
+          </a>
+        </Link>
+        <Link href="https://www.linkedin.com/in/aasim-shaikh-8b7a57176/">
+          <a>
+            <AiFillLinkedin className="w-6 h-6" />
+          </a>
+        </Link>
       </div>
     </footer>
   );
