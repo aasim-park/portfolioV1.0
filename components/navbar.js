b7e79d98d7f1bcd7e5c86dc7a5ec8d79d24ad5b0
Aasim Shaikh <asimshaikh1993@gmail.com>
2022-02-21 20:31:25 +0530

    Added icon for logo

--- components/navbar.js
+++ components/navbar.js
@@ -1,11 +1,16 @@
 import Link from "next/link";
+import { GiTunisia } from "react-icons/gi";
 
 const NavBar = () => {
   return (
     <nav className="flex flex-row">
       {/* Logo */}
-      <div className="flex justify-center w-1/5 h-8 m-4">
-        <a className="text-2xl">Logo</a>
+      <div className="flex w-1/5 h-8 px-20 m-4">
+        <Link href="/">
+          <a>
+            <GiTunisia className="w-11 h-11" />
+          </a>
+        </Link>
       </div>
       {/* right side navigation bar */}
       <div className="flex justify-end w-4/5 gap-10 pr-16 m-4">
