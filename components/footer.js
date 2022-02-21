6b8fabc6637fa5ffa6453ae4533ecee5bd4ce4b6
Aasim Shaikh <asimshaikh1993@gmail.com>
2022-02-21 19:09:26 +0530

    Blog page done

--- components/footer.js
+++ components/footer.js
@@ -4,27 +4,27 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="flex flex-col border-y-4">
-      {/* Copyright */}
-      <div className="flex justify-center p-2 m-2">
-        <p>Copyright &#169; 2022 </p>
-      </div>
       {/* Icons */}
-      <div className="flex justify-center py-4 gap-7">
+      <div className="flex justify-center pt-3 gap-7">
         <Link href="https://github.com/aasim-park?tab=repositories">
           <a>
-            <AiFillGithub className="w-6 h-6" />
+            <AiFillGithub className="w-8 h-8" />
           </a>
         </Link>
         <Link href="https://www.instagram.com/aasim08/">
           <a>
-            <AiFillInstagram className="w-6 h-6" />
+            <AiFillInstagram className="w-8 h-8" />
           </a>
         </Link>
         <Link href="https://www.linkedin.com/in/aasim-shaikh-8b7a57176/">
           <a>
-            <AiFillLinkedin className="w-6 h-6" />
+            <AiFillLinkedin className="w-8 h-8" />
           </a>
         </Link>
+      </div>
+      {/* Copyright */}
+      <div className="flex justify-center p-1 m-1">
+        <p>Copyright &#169; 2022 </p>
       </div>
     </footer>
   );
--- pages/blog.js
+++ pages/blog.js
@@ -1,15 +1,86 @@
+import Head from "next/head";
+
 const Blog = () => {
   return (
     <>
+      <Head>
+        <title>Portfolio | Blog</title>
+      </Head>
       {/* Wrapper container */}
-      <div className="flex p-20 m-24">
+      <div className="flex flex-col gap-10 p-20 m-24">
         {/* Title-Blog */}
         <div>
-          <p>Blog</p>
+          <p className="font-bold text-7xl">Blog</p>
         </div>
         {/* Lists */}
-        <div>
-          <div>Columns</div>
+        <div className="flex flex-col gap-24">
+          {/* List title */}
+          <div className="flex flex-col gap-2">
+            <div>
+              <p className="text-2xl">UI Interactions of the week</p>
+            </div>
+            {/* Date | tags */}
+            <div className="flex flex-row gap-3">
+              <div>
+                <p>12 Feb 2019</p>
+              </div>
+              <div>
+                <p>Express, Handlebars</p>
+              </div>
+            </div>
+            {/* Discrption */}
+            <div>
+              <p>
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint. Velit officia consequat duis enim velit mollit.
+                Exercitation veniam consequat sunt nostrud amet.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div>
+              <p className="text-2xl">UI Interactions of the week</p>
+            </div>
+            {/* Date | tags */}
+            <div className="flex flex-row gap-3">
+              <div>
+                <p>12 Feb 2019</p>
+              </div>
+              <div>
+                <p>Express, Handlebars</p>
+              </div>
+            </div>
+            {/* Discrption */}
+            <div>
+              <p>
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint. Velit officia consequat duis enim velit mollit.
+                Exercitation veniam consequat sunt nostrud amet.
+              </p>
+            </div>
+          </div>{" "}
+          <div className="flex flex-col gap-2">
+            <div>
+              <p className="text-2xl">UI Interactions of the week</p>
+            </div>
+            {/* Date | tags */}
+            <div className="flex flex-row gap-3">
+              <div>
+                <p>12 Feb 2019</p>
+              </div>
+              <div>
+                <p>Express, Handlebars</p>
+              </div>
+            </div>
+            {/* Discrption */}
+            <div>
+              <p>
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint. Velit officia consequat duis enim velit mollit.
+                Exercitation veniam consequat sunt nostrud amet.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
