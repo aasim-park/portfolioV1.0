df6cccc0a9f306f720e3763be530acf8f485a7bd
Aasim Shaikh <asimshaikh1993@gmail.com>
2022-02-22 14:25:32 +0530

    Updated contact page for mobile

--- pages/contact.js
+++ pages/contact.js
@@ -6,12 +6,20 @@ const Contact = () => {
       <Head>
         <title>Portfolio | Contact</title>
       </Head>
-      <div>
-        <h1 className="font-bold">Contacts</h1>
+      <div className="flex flex-col gap-3 ml-8 mr-3">
+        <p className="font-bold">Contacts</p>
         <p>
-          Hello this is Aasim. Hope you are having a wonderful daycheck below
-          link so we can collaborate.
+          Hello this is Aasim.{" "}
+          <a className="italic underline decoration-2 decoration-indigo-500">
+            {" "}
+            Hope you are having a wonderful day{" "}
+          </a>
+          check below link so we can collaborate.
         </p>
+        <div>
+          <p>Mobile number:+91-9920816963 </p>
+          <p>Email Id: asimshaikh1993@gmail.com</p>
+        </div>
       </div>
     </>
   );
