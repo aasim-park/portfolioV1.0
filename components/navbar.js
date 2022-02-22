2ead252dcaaeee31bd4369683956caea307dce5f
Aasim Shaikh <asimshaikh1993@gmail.com>
2022-02-22 12:30:15 +0530

    fixend jumping and zooming of NavBar

--- components/navbar.js
+++ components/navbar.js
@@ -5,15 +5,9 @@ import Head from "next/head";
 const NavBar = () => {
   return (
     <>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        ></meta>
-      </Head>
-      <nav className="flex flex-row">
+      <nav className="flex overflow-hidden">
         {/* Logo */}
-        <div className="flex invisible m-4 lg:w-1/5 lg:px-20 lg:visible">
+        <div className="hidden m-4 lg:w-1/5 lg:px-20 lg:flex lg:visible">
           <Link href="/">
             <a>
               <GiTunisia className="w-11 h-11" />
@@ -21,7 +15,7 @@ const NavBar = () => {
           </Link>
         </div>
         {/* right side navigation bar */}
-        <div className="flex items-center w-full gap-10 m-2 lg:m-4 lg:w-4/5 lg:pr-16 lg:justify-end">
+        <div className="flex items-center gap-10 px-4 m-4 lg:w-4/5 lg:pr-16 lg:justify-end">
           <Link href="/">
             <a>Home</a>
           </Link>
